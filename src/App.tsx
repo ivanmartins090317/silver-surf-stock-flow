fbf0f7f0bbc3452e29259b864f413052709b7370
Ivan Martins <ivanmartins.ilha@gmail.com>
2025-06-26 11:19:14 -0300

    fix: erro on render aplication, fix authProvider on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { Layout } from "./components/Layout";
+import {Toaster} from "@/components/ui/toaster";
+import {Toaster as Sonner} from "@/components/ui/sonner";
+import {TooltipProvider} from "@/components/ui/tooltip";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {SidebarProvider} from "@/components/ui/sidebar";
+import {Layout} from "./components/Layout";
 import Index from "./pages/Index";
 import Clients from "./pages/Clients";
 import Materials from "./pages/Materials";
@@ -14,7 +13,8 @@ import Accessories from "./pages/Accessories";
 import QRScan from "./pages/QRScan";
 import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
-import { ProtectedRoute } from "./components/ProtectedRoute";
+import {ProtectedRoute} from "./components/ProtectedRoute";
+import {AuthProvider} from "@/hooks/useAuth";
 
 const queryClient = new QueryClient();
 
@@ -24,24 +24,29 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <SidebarProvider>
-          <Routes>
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/qr-scan/:accessoryId" element={<QRScan />} />
-            <Route path="/" element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }>
-              <Route index element={<Index />} />
-              <Route path="clients" element={<Clients />} />
-              <Route path="materials" element={<Materials />} />
-              <Route path="products" element={<Products />} />
-              <Route path="accessories" element={<Accessories />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </SidebarProvider>
+        <AuthProvider>
+          <SidebarProvider>
+            <Routes>
+              <Route path="/auth" element={<Auth />} />
+              <Route path="/qr-scan/:accessoryId" element={<QRScan />} />
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <Layout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Index />} />
+                <Route path="clients" element={<Clients />} />
+                <Route path="materials" element={<Materials />} />
+                <Route path="products" element={<Products />} />
+                <Route path="accessories" element={<Accessories />} />
+              </Route>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </SidebarProvider>
+        </AuthProvider>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
